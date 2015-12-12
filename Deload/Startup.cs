0a9da2192c77fcafddfa2e39905eab9eba0b410f
Cynthia Subakat <cynn.d.s@gmail.com>
2015-12-12 16:24:21 +1100

    Initial commit

---
+++ Deload/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Deload.Startup))]
+namespace Deload
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
